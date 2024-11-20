3e780158d21593abaa52f7774e4d3ae30657cc2a
Ishita Gupta <66175970+ishitaaagupta@users.noreply.github.com>
2024-11-20 18:30:13 +0530

    code cleanup

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import { dbConnection } from "./database/dbConnection.js";
-import { errorMiddleware } from './middlewares/error.js';
-import reservationRouter from './routes/reservationRoute.js';
+import { errorMiddleware } from "./middlewares/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
 
-const app = express()
-dotenv.config({path: './config/config.env'});
+const app = express();
+dotenv.config({ path: "./config/config.env" });
 
-
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["POST"],
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST"],
     credentials: true,
-})
+  })
 );
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/reservation', reservationRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/reservation", reservationRouter);
 
 dbConnection();
 
